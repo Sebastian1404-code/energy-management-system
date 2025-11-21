c6c567ecf9403ce1c6b66dce4419366151308b35
Sebastian1404-code <muresan.sebastian2000@gmail.com>
2025-11-21 13:24:47 +0200

    added endpoint for update devices

--- DeviceManagement/src/main/java/distributedSystem/DeviceManagement/service/DeviceService.java
+++ DeviceManagement/src/main/java/distributedSystem/DeviceManagement/service/DeviceService.java
@@ -2,13 +2,17 @@ package distributedSystem.DeviceManagement.service;
 
 import distributedSystem.DeviceManagement.dto.DeviceRequest;
 import distributedSystem.DeviceManagement.dto.DeviceResponse;
+import distributedSystem.DeviceManagement.dto.UpdateDeviceMetaRequest;
 import distributedSystem.DeviceManagement.model.Device;
 import distributedSystem.DeviceManagement.model.DeviceUserRef;
 import distributedSystem.DeviceManagement.repository.DeviceRepository;
 import distributedSystem.DeviceManagement.repository.DeviceUserRefRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.notFound;
 
 @Service
 public class DeviceService {
@@ -77,7 +81,31 @@ public class DeviceService {
     }
 
 
+
+    @Transactional
+    public DeviceResponse updateNameAndConsumption(Long id, UpdateDeviceMetaRequest req) {
+        Device device = deviceRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Device not found with id: " + id));
+
+        device.setName(req.getName());
+        device.setMaximConsumptionValue(req.getMaximConsumptionValue());
+        return toResponse(device);
+    }
+
+
     public void delete(Long id) {
         deviceRepo.deleteById(id);
     }
+
+
+    private DeviceResponse toResponse(Device d) {
+        Long userId = (d.getUserRef() != null) ? d.getUserRef().getUser_id() : null;
+        return DeviceResponse.builder()
+                .id(d.getId())
+                .name(d.getName())
+                .maximConsumptionValue(d.getMaximConsumptionValue())
+                .userId(userId)
+                .build();
+    }
+
 }
